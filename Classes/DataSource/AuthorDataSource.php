631678b935444ab923835f2121dc3532421c8a90
Jon Uhlmann <me@jonnitto.ch>
2021-04-15 15:24:07 +0200

    Revert "Update: Set return type and remove not used arguments"
    This reverts commit a499fd5a16387e85d1fe1d35abd6ad30ac3b37e8.

--- Classes/DataSource/AuthorDataSource.php
+++ Classes/DataSource/AuthorDataSource.php
@@ -6,6 +6,7 @@ use Neos\Flow\Persistence\PersistenceManagerInterface;
 use Neos\Flow\Security\Context as SecurityContext;
 use Neos\Neos\Domain\Service\UserService;
 use Neos\Neos\Service\DataSource\AbstractDataSource;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Flow\Security\Account;
 use Neos\Flow\Security\Policy\Role;
 use Neos\Neos\Domain\Model\User;
@@ -42,9 +43,11 @@ class AuthorDataSource extends AbstractDataSource
     protected $persistenceManager;
 
     /**
-     * @return array JSON serializable data
+     * @param NodeInterface $node The node that is currently edited (optional)
+     * @param array $arguments Additional arguments (key / value)
+     * @return mixed JSON serializable data
      */
-    public function getData(): array
+    public function getData(NodeInterface $node = null, array $arguments = [])
     {
         $users = $this->userService->getUsers();
         $data = [];
